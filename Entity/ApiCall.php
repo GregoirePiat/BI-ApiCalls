fd8468b17756f80abbb9f00f59aecebaae5d9ba1
GregoirePiat <gregoire.piat@outlook.fr>
2017-09-08 16:52:56 +0200

    Handle credentials and set option on curl call

--- Entity/ApiCall.php
+++ Entity/ApiCall.php
@@ -32,12 +32,33 @@ class ApiCall
     protected $fileName;
 
     /**
-     * ApiCall constructor.
-     * @param string    $apiUrl
-     * @param string    $serviceName
+     * @var string
      */
-    public function __construct($apiUrl, $serviceName)
+    protected $username;
+
+    /**
+     * @var string
+     */
+    protected $password;
+
+    /**
+     * @var string
+     */
+    protected $useCredentials;
+
+    /**
+     * ApiCall constructor.
+     * @param string        $apiUrl
+     * @param string        $serviceName
+     * @param boolean       $useCredentials
+     */
+    public function __construct($apiUrl, $serviceName, $useCredentials = false)
     {
+        $this->useCredentials = $useCredentials;
+
+        if (isset($this->useCredentials))
+            $this->extractCredentials();
+
         $this->apiUrl = $apiUrl;
         $this->serviceName = $serviceName;
         $this->setTimezone();
@@ -55,6 +76,10 @@ class ApiCall
         curl_setopt($session,CURLOPT_URL, $this->apiUrl);
         curl_setopt($session,CURLOPT_RETURNTRANSFER,true);
         curl_setopt($session,CURLOPT_HEADER, false);
+
+        if ($this->useCredentials)
+            curl_setopt($session, CURLOPT_USERPWD, $this->username . ":" . $this->password);
+
         $result = curl_exec($session);
 
         return $result;
@@ -91,5 +116,18 @@ class ApiCall
 
     private function computeDirectoryPath(){
         $this->directoryPath = dirname(__FILE__) . "/../data/" . $this->serviceName;
+    }
+
+    private function extractCredentials(){
+
+        $credentialsPath = dirname(__FILE__) . "/../Credentials/Credentials.json";
+
+        if (file_exists($credentialsPath)) {
+            $jsonContent = json_decode(file_get_contents($credentialsPath));
+            if (isset($jsonContent->username))
+                $this->username = $jsonContent->username;
+            if (isset($jsonContent->password))
+                $this->password = $jsonContent->password;
+        }
     }
 }
--- Scripts/Run.php
+++ Scripts/Run.php
@@ -8,7 +8,6 @@ require_once(dirname(__FILE__) . "/../Entity/ApiCall.php");
 /** Call Velov API */
 $velovApi = new \ApiCalls\ApiCall("https://download.data.grandlyon.com/ws/rdata/jcd_jcdecaux.jcdvelov/all.json", "VELOV");
 
-// Set credentials when available
-//$trafficApi = new \ApiCalls\ApiCall("https://download.data.grandlyon.com/ws/rdata/pvo_patrimoine_voirie.pvotrafic/all.json?compact=false&start=1001&maxfeatures=2000", "TRAFFIC");
+$trafficApi = new \ApiCalls\ApiCall("https://download.data.grandlyon.com/ws/rdata/pvo_patrimoine_voirie.pvotrafic/all.json?compact=false&start=1001&maxfeatures=2000", "TRAFFIC", true);
 
 ?>
