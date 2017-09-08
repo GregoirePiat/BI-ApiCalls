6b001ea4875910659ec46ab03c7e5a3673595c09
GregoirePiat <gregoire.piat@outlook.fr>
2017-09-08 17:29:06 +0200

    Add new sources

--- Scripts/Run.php
+++ Scripts/Run.php
@@ -6,8 +6,14 @@
 require_once(dirname(__FILE__) . "/../Entity/ApiCall.php");
 
 /** Call Velov API */
-$velovApi = new \ApiCalls\ApiCall("https://download.data.grandlyon.com/ws/rdata/jcd_jcdecaux.jcdvelov/all.json", "VELOV");
+$velovApi = new \ApiCalls\ApiCall("https://download.data.grandlyon.com/wfs/rdata?SERVICE=WFS&VERSION=2.0.0&outputformat=GEOJSON&maxfeatures=20000&request=GetFeature&typename=jcd_jcdecaux.jcdvelov&SRSNAME=urn:ogc:def:crs:EPSG::4171", "VELOV");
 
-$trafficApi = new \ApiCalls\ApiCall("https://download.data.grandlyon.com/ws/rdata/pvo_patrimoine_voirie.pvotrafic/all.json?compact=false&start=1001&maxfeatures=2000", "TRAFFIC", true);
+$trafficApi = new \ApiCalls\ApiCall("https://download.data.grandlyon.com/wfs/rdata?SERVICE=WFS&VERSION=2.0.0&outputformat=GEOJSON&maxfeatures=20000&request=GetFeature&typename=pvo_patrimoine_voirie.pvotrafic&SRSNAME=urn:ogc:def:crs:EPSG::4171", "TRAFFIC", true);
+
+$parkingApi = new \ApiCalls\ApiCall("https://download.data.grandlyon.com/wfs/rdata?SERVICE=WFS&VERSION=2.0.0&outputformat=GEOJSON&maxfeatures=20000&request=GetFeature&typename=pvo_patrimoine_voirie.pvoparkingtr&SRSNAME=urn:ogc:def:crs:EPSG::4171", "PARKING", true);
+
+$incidentsApi = new \ApiCalls\ApiCall("https://download.data.grandlyon.com/wfs/rdata?SERVICE=WFS&VERSION=2.0.0&outputformat=GEOJSON&maxfeatures=20000&request=GetFeature&typename=pvo_patrimoine_voirie.pvoevenement&SRSNAME=urn:ogc:def:crs:EPSG::4171", "INCIDENTS", true);
+
+$parcRelaisApi = new \ApiCalls\ApiCall("https://download.data.grandlyon.com/wfs/rdata?SERVICE=WFS&VERSION=2.0.0&outputformat=GEOJSON&maxfeatures=30&request=GetFeature&typename=tcl_sytral.tclparcrelaistr&SRSNAME=urn:ogc:def:crs:EPSG::4171", "PARCSRELAIS", true);
 
 ?>
